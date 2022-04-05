40f48892f2d509d2d7e370545f3480ee30ff33b2
BT_1E10_34 <sample@bths.edu>
2022-04-05 10:09:20 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println(factorial(10));
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a number to start: ");
+        int start = input.nextInt();
+        System.out.println(countdown(start));
+
     }
 
     public static int factorial(int num){
@@ -9,5 +15,12 @@ public class Main {
             product *= i;
         }
         return product;
+    }
+
+    public static String countdown(int num){
+        if(num == 0){
+            return "Blastoff!";
+        }
+        return num  + " " + countdown(num - 1);
     }
 }
